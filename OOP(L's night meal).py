cb246f37d0085ad00b397b8c9615f7c995f05d30
Hellenivy <hellenakinyiivy@gmail.com>
2020-07-18 07:27:55 +0300

    first commit

---
+++ OOP(L's night meal).py
@@ -0,0 +1,25 @@
+print('What would you like to have super tonight?')
+
+food = input("chicken")
+
+print(food + ', nope we had that last week, anything else you would like?')
+
+input()
+
+print('naah, don\'t make it look fancy - what else can we have?')
+
+input()
+
+print('that\'s boring. What do we have in the fridge?')
+
+input()
+
+print('we could have that tomorrow. What shall we have tonight?')
+
+input()
+
+print('Iveey.Ask me what I want')
+
+input()
+
+print('not bothered')
